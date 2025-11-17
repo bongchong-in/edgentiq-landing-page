9c64c23e5b2e5e29692780ed5d60eb6e8660257e
bongchong-in <madhushree@bongchong.in>
2025-11-17 16:43:38 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,12 +6,14 @@ import Problem from './components/Problem';
 import Solution from './components/Solution';
 import Process from './components/Process';
 import About from './components/About';
+import WhoWeServe from './components/WhoWeServe';
+import Specialties from './components/Specialties';
 import FinalCTA from './components/FinalCTA';
 import Footer from './components/Footer';
 import BookingPage from './components/BookingPage';
 
 const App: React.FC = () => {
-  const [route, setRoute] = useState(() => window.location.hash || '#/');
+  const [route, setRoute] = useState(window.location.hash || '#/');
 
   useEffect(() => {
     const handleHashChange = () => {
@@ -19,12 +21,6 @@ const App: React.FC = () => {
     };
 
     window.addEventListener('hashchange', handleHashChange);
-
-    // If the hash was empty on load, update the URL to match the default state.
-    // The event listener will handle any state changes if necessary.
-    if (window.location.hash !== route) {
-      window.location.hash = route;
-    }
 
     return () => {
       window.removeEventListener('hashchange', handleHashChange);
@@ -43,6 +39,8 @@ const App: React.FC = () => {
         <Solution />
         <Process />
         <About />
+        <WhoWeServe />
+        <Specialties />
         <FinalCTA />
       </>
     );
